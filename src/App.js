010e1305e64ef05589b4de4f6515e979a06ccaaf
Juliana Chagas <julianavrchagas@gmail.com>
2023-02-06 11:43:51 -0300

    feat: add main app script

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import GlobalStyles from './components/styles/Global';
+import content from './content';
+import Card from './components/Card';
+import Section from './components/Section';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+
+const theme = {
+  colors: {
+    primary: 'hsl(322, 100%, 66%)',
+    paleCyan: ' hsl(193, 100%, 96%)',
+    darkCyan: 'hsl(192, 100%, 9%)',
+    grayishBlue: 'hsl(208, 11%, 55%)',
+  },
+  mobile: '768px',
+};
+
 function App() {
   return (
-    <div>
-      <h1>Huddle</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Hero />
+      <Section>
+        {content.map(item => (
+          <Card key={item.id} {...item} />
+        ))}
+      </Section>
+      <Banner />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
